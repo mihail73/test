06c9a194b7ae5fe352a8aa42e2090147561ed956
mihail73 <36410686+mihail73@users.noreply.github.com>
2018-05-25 23:11:21 +0300

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,5 +1,5 @@
 sudo ln -s /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
 sudo rm /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
-#sudo ln -s /home/box/web/etc/gunicorn.conf /etc/gunicorn.d/test
-#sudo /etc/init.d/gunicorn restart
+sudo gunicorn -c /home/box/web/etc/gunicorn.conf hello:wsgi_application
+sudo gunicorn -c /home/box/web/etc/gunicorn-django.conf ask.wsgi:application
